734a614f2869ed745b3137c287051437aa20e410
yangwenzhi1 <76517317+yangwenzhi1@users.noreply.github.com>
2020-12-23 21:45:24 +0800

    Add files via upload

---
+++ ABCDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABCDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
